90db2965f8e711f99027ff53a9238d74ae872dca
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-12-04 01:57:44 +0300

    gradient descent adding testing and update dataset

--- optimization.c
+++ optimization.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <time.h>
 
-#define N 24 // Görüntü boyutu NxN
+#define N 28 // Görüntü boyutu NxN
 
 void read_csv(const char *filename, double **matrix) {
     FILE *file = fopen(filename, "r");
@@ -13,7 +13,7 @@ void read_csv(const char *filename, double **matrix) {
     }
 	int i,j;
     for (i = 0; i < 200; i++) {
-        for (j = 0; j < 577; j++) {
+        for (j = 0; j < 785; j++) {
             if (fscanf(file, "%lf,", &matrix[i][j]) != 1) {
                 fprintf(stderr, "Dosya formati hatali veya beklenmeyen veri\n");
                 fclose(file);
@@ -27,7 +27,7 @@ void read_csv(const char *filename, double **matrix) {
 
 void generate_w(double *w,double baslangic){
 	int i;
-    for(i=0;i<577;i++){
+    for(i=0;i<785;i++){
         w[i] = baslangic;
     }
 
@@ -54,36 +54,35 @@ void hedef_duzenleme(int *hedef){
 void egitim_test_ayristirma(double **x,double **x_egitim,double **x_test){
     int i,j;
     for(i=0; i<80; i++){
-        for(j=0;j<577;j++){
+        for(j=0;j<785;j++){
             x_egitim[i][j] = x[i][j]; 
         }
         
     }
     for(i=100; i<180; i++){
-        for(j=0;j<577;j++){
+        for(j=0;j<785;j++){
             x_egitim[i-20][j] = x[i][j]; 
         }
         
     }
     for(i=80; i<100; i++){
-        for(j=0;j<577;j++){
+        for(j=0;j<785;j++){
             x_test[i-80][j] = x[i][j]; 
         }
         
     }
     for(i=180; i<200; i++){
-        for(j=0;j<577;j++){
+        for(j=0;j<785;j++){
             x_test[i-160][j] = x[i][j]; 
         }
         
     }
 
-    printf("buraya kadar calisti mi");
 }
 
 void w_izleme_duzenleme(double *w, double **w_izleme, int iterasyon ){
     int i;
-    for(i=0;i<577;i++){
+    for(i=0;i<785;i++){
         w_izleme[iterasyon][i] = w[i];
     }
 }
@@ -92,7 +91,7 @@ void gradient_descent_w_duzenleme(double *w,double **x_egitim,int *hedef ){
     int i,j;
     double sum,eps=0.001 ;
 
-    for(j=0;j<577;j++){
+    for(j=0;j<785;j++){
 
         sum = 0;
 
@@ -117,11 +116,12 @@ void gradient_descent_w_duzenleme(double *w,double **x_egitim,int *hedef ){
 void gradient_descent(double **x,double **x_egitim,double **x_test,double *w,double **w_izleme,int *hedef){
     int i,j,iterasyon;
     double sum;
+    int sonuc[40];
 
 
 
-    for(i=0;i<577;i++){
-        w_izleme[0][577] = w[i];
+    for(i=0;i<785;i++){
+        w_izleme[0][785] = w[i];
     }
 
     for(iterasyon = 1;iterasyon <101;iterasyon++){
@@ -129,6 +129,29 @@ void gradient_descent(double **x,double **x_egitim,double **x_test,double *w,dou
         w_izleme_duzenleme(w,w_izleme,iterasyon);
     }
     
+    for(i=0;i<40;i++){
+        sum =0;
+        for(j=0;j<785;j++){
+            sum += x_test[i][j]*w[j]; 
+        }
+
+        if(i<20){
+            
+            if(tanh(sum) >= 0.5){
+                sonuc[i] = 1;
+            }else{
+                sonuc[i] = 0;
+            }
+
+
+        }else{
+            if(tanh(sum) <= -0.5){
+                sonuc[i] = 1;
+            }else{
+                sonuc[i] = 0;
+            }
+        }
+    }
     
 }
 
@@ -138,35 +161,35 @@ int main(){
     int i;
     double **x = (double **)malloc(200*sizeof(double *));
     for(i=0;i<200;i++){
-        x[i] = (double *)malloc(577*sizeof(double));
+        x[i] = (double *)malloc(785*sizeof(double));
     }
-    double *w = malloc(577*sizeof(double));
+    double *w = malloc(785*sizeof(double));
     double **x_egitim;
     x_egitim = (double **)malloc(160*sizeof(double *));
     for(i=0;i<160;i++){
-        x_egitim[i] = (double *)malloc(577*sizeof(double));
+        x_egitim[i] = (double *)malloc(785*sizeof(double));
     }
     double **x_test;
     x_test = (double **)malloc(40*sizeof(double *));
     for(i=0;i<40;i++){
-        x_test[i] = (double *)malloc(577*sizeof(double));
+        x_test[i] = (double *)malloc(785*sizeof(double));
     }
     double **w_izleme;
     w_izleme = (double **)malloc(101*sizeof(double *));
     for(i=0;i<40;i++){
-        w_izleme[i] = (double *)malloc(577*sizeof(double));
+        w_izleme[i] = (double *)malloc(785*sizeof(double));
     }
     int *hedef = malloc(200*sizeof(int));
 
 
-    //const char *filename = "data.csv";
+    const char *filename = "data.csv";
 
     hedef_duzenleme(hedef);
-    // read_csv(filename, x);
+    read_csv(filename, x);
     egitim_test_ayristirma(x,x_egitim,x_test);
+    gradient_descent(x,x_egitim,x_test,w,w_izleme,hedef);
+    
 
-
-    printf("Kod calisiyor");
 
     return 0;
 }
