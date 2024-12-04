62051ca18e9f11825dda45ba2719ca26929074ac
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-12-04 10:32:43 +0300

    Syntax Edits function names and variable names have been changed to English

--- optimization.c
+++ optimization.c
@@ -33,35 +33,35 @@ void generate_w(double *w,double baslangic){
 
 }
 
-double loss_turevi(double w,double x,int hedef){
+double derivative_loss(double w,double x,int y){
 
-    return -2*(hedef-tanh(w*x))*(1-pow(tanh(w*x),2))*x;
+    return -2*(y-tanh(w*x))*(1-pow(tanh(w*x),2))*x;
 
 
 }
 
-void hedef_duzenleme(int *hedef){
+void y_edit(int *y){
 	int i;
     for(i=0;i<100;i++){
-        hedef[i] = 1;
+        y[i] = 1;
     }
     for(i = 100;i<200;i++){
-        hedef[i] = -1;
+        y[i] = -1;
     }
 
 }
 
-void egitim_test_ayristirma(double **x,double **x_egitim,double **x_test){
+void separation_train_Test(double **x,double **x_train,double **x_test){
     int i,j;
     for(i=0; i<80; i++){
         for(j=0;j<785;j++){
-            x_egitim[i][j] = x[i][j]; 
+            x_train[i][j] = x[i][j]; 
         }
         
     }
     for(i=100; i<180; i++){
         for(j=0;j<785;j++){
-            x_egitim[i-20][j] = x[i][j]; 
+            x_train[i-20][j] = x[i][j]; 
         }
         
     }
@@ -80,14 +80,14 @@ void egitim_test_ayristirma(double **x,double **x_egitim,double **x_test){
 
 }
 
-void w_izleme_duzenleme(double *w, double **w_izleme, int iterasyon ){
+void w_history_edit(double *w, double **w_history, int iteration ){
     int i;
     for(i=0;i<785;i++){
-        w_izleme[iterasyon][i] = w[i];
+        w_history[iteration][i] = w[i];
     }
 }
 
-void gradient_descent_w_duzenleme(double *w,double **x_egitim,int *hedef ){
+void gradient_descent_w_edit(double *w,double **x_train,int *y ){
     int i,j;
     double sum,eps=0.001 ;
 
@@ -98,9 +98,9 @@ void gradient_descent_w_duzenleme(double *w,double **x_egitim,int *hedef ){
         for(i=0;i<160;i++){
             
             if(i<80){
-                sum += loss_turevi(w[j],x_egitim[i][j],hedef[i]);
+                sum += derivative_loss(w[j],x_train[i][j],y[i]);
             }else{
-                sum += loss_turevi(w[j],x_egitim[i][j],hedef[i+20]);
+                sum += derivative_loss(w[j],x_train[i][j],y[i+20]);
             }
             
         }
@@ -113,19 +113,19 @@ void gradient_descent_w_duzenleme(double *w,double **x_egitim,int *hedef ){
 
 }
 
-void gradient_descent(double **x,double **x_egitim,double **x_test,double *w,double **w_izleme,int *hedef){
-    int i,j,iterasyon;
+void gradient_descent(double **x,double **x_train,double **x_test,double *w,double **w_history,int *y){
+    int i,j,iteration;
     double sum;
-    int sonuc[40];
+    int result[40];
 
 
 
     for(i=0;i<785;i++){
-        w_izleme[0][785] = w[i];
+        w_history[0][785] = w[i];
     }
-    for(iterasyon = 1;iterasyon <101;iterasyon++){
-        gradient_descent_w_duzenleme(w,x_egitim,hedef);
-        w_izleme_duzenleme(w,w_izleme,iterasyon);
+    for(iteration = 1;iteration <101;iteration++){
+        gradient_descent_w_edit(w,x_train,y);
+        w_history_edit(w,w_history,iteration);
     }
     
     for(i=0;i<40;i++){
@@ -137,19 +137,51 @@ void gradient_descent(double **x,double **x_egitim,double **x_test,double *w,dou
         if(i<20){
             
             if(tanh(sum) >= 0.5){
-                sonuc[i] = 1;
+                result[i] = 1;
             }else{
-                sonuc[i] = 0;
+                result[i] = 0;
             }
 
 
         }else{
             if(tanh(sum) <= -0.5){
-                sonuc[i] = 1;
+                result[i] = 1;
             }else{
-                sonuc[i] = 0;
+                result[i] = 0;
             }
         }
+    }
+    
+}
+
+void Adam_Algorithm(double **x,double *w,double **x_train,double **w_history,int *y){
+    double learningRate = 0.001,b1 = 0.9,b2 = 0.999,e = pow(10,-8),gt,mt=0,vt=0;
+    int i,j,iteration;
+
+    for(i=0;i<785;i++){
+        w_history[0][785] = w[i];
+    }
+    for(iteration = 1;iteration<101;iteration++){
+        j = rand() % 160;
+
+        for(i=0;i<785;i++){
+            if(j<80){
+                gt = derivative_loss(w[i],x_train[j][i],y[j]);
+            }else{
+                gt = derivative_loss(w[i],x_train[j][i],y[i+20]);
+            }
+
+            mt = (b1 * mt) + (1.0 - b1)*gt;
+            vt = (b2 * vt) + (1.0 - b2)*pow(gt,2);
+
+            w[i] -= (learningRate*mt)/sqrt(vt + e);
+            
+        }
+
+        w_history_edit(w,w_history,iteration);
+
+
+
     }
     
 }
@@ -163,28 +195,28 @@ int main(){
         x[i] = (double *)calloc(785,sizeof(double));
     }
     double *w = calloc(785,sizeof(double));
-    double **x_egitim;
-    x_egitim = (double **)calloc(160,sizeof(double *));
+    double **x_train;
+    x_train = (double **)calloc(160,sizeof(double *));
     for(i=0;i<160;i++){
-        x_egitim[i] = (double *)calloc(785,sizeof(double));
+        x_train[i] = (double *)calloc(785,sizeof(double));
     }
     double **x_test;
     x_test = (double **)calloc(40,sizeof(double *));
     for(i=0;i<40;i++){
         x_test[i] = (double *)calloc(785,sizeof(double));
     }
-    double **w_izleme;
-    w_izleme = (double **)calloc(101,sizeof(double *));
+    double **w_history;
+    w_history = (double **)calloc(101,sizeof(double *));
     for(i=0;i<101;i++){
-        w_izleme[i] = (double *)calloc(785,sizeof(double));
+        w_history[i] = (double *)calloc(785,sizeof(double));
     }
-    int *hedef = calloc(200,sizeof(int));
+    int *y = calloc(200,sizeof(int));
     //const char *filename = "data.csv";
 
-    hedef_duzenleme(hedef);
+    y_edit(y);
     //read_csv(filename, x);
-    egitim_test_ayristirma(x,x_egitim,x_test);
-    gradient_descent(x,x_egitim,x_test,w,w_izleme,hedef);
+    separation_train_Test(x,x_train,x_test);
+    gradient_descent(x,x_train,x_test,w,w_history,y);
     
     printf("calisiyo");
     return 0;
