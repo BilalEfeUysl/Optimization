cd7f0f89b38365789dec3246167c6f410ea57dd1
BilalEfeUysl <bilalefeuysalbs@gmail.com>
2024-12-04 23:55:11 +0300

    Stochastic Gradiant Descent etc added stochastic and added a function to read data from the file

--- optimization.c
+++ optimization.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
-#define N 28 // Görüntü boyutu NxN
+#define N 28 
+#define EPS 0.001
+#define LINE_LENGTH 1024 
 
 void read_csv(const char *filename, double **matrix);
 void generate_w(double *w,double baslangic);
@@ -13,24 +16,31 @@ void separation_train_Test(double **x,double **x_train,double **x_test);
 void w_history_edit(double *w, double **w_history, int iteration );
 void gradient_descent_w_edit(double *w,double **x_train,int *y );
 void gradient_descent(double **x,double **x_train,double **x_test,double *w,double **w_history,int *y);
-void Adam_Algorithm(double **x,double *w,double **x_train,double **w_history,int *y);
+void Adam_Algorithm(double **x,double *w,double **x_train,double **x_test,double **w_history,int *y);
 
 
 void read_csv(const char *filename, double **matrix) {
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Dosya acilamadi");
-        exit(EXIT_FAILURE);
+    if (!file) {
+        printf("Dosya acilamadi: %s\n", filename);
+        return;
     }
-	int i,j;
-    for (i = 0; i < 200; i++) {
-        for (j = 0; j < 785; j++) {
-            if (fscanf(file, "%lf,", &matrix[i][j]) != 1) {
-                fprintf(stderr, "Dosya formati hatali veya beklenmeyen veri\n");
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
+
+    char line[LINE_LENGTH];
+    int row = 0;
+
+    // Dosyayı satır satır oku
+    while (fgets(line, sizeof(line), file) && row < 200) {
+        int col = 0;
+        char *token = strtok(line, ";");
+
+        // Satırdaki her sütunu virgülle ayırarak okuma
+        while (token && col < 785) {
+            matrix[row][col] = atof(token);  // Veriyi sayıya çevirerek matrise ekle
+            token = strtok(NULL, ";");
+            col++;
         }
+        row++;
     }
 
     fclose(file);
@@ -100,7 +110,8 @@ void w_history_edit(double *w, double **w_history, int iteration ){
 
 void gradient_descent_w_edit(double *w,double **x_train,int *y ){
     int i,j;
-    double sum,eps=0.001 ;
+    double sum;
+    
 
     for(j=0;j<785;j++){
 
@@ -116,11 +127,28 @@ void gradient_descent_w_edit(double *w,double **x_train,int *y ){
             
         }
 
-        w[j] = w[j] - eps*(sum/160.0) ;
+        w[j] = w[j] - EPS*(sum/160.0) ;
 
 
         
     }
+
+}
+void stochastic_gradiant_w_edit(double *w,double **x_train,int *y){
+    int i,j;
+    double derivative;
+
+    i = rand() % 160;
+
+    for(j=0;j<785;j++){
+        if(i<80){
+            derivative = derivative_loss(w[j],x_train[i][j],y[i]);   
+        }else{
+            derivative = derivative_loss(w[j],x_train[i][j],y[i+20]);
+        }
+        w[j] = w[j] - EPS*(derivative);
+    }
+
 
 }
 
@@ -148,7 +176,7 @@ double calculate_loss(double *w,double **x_train,int *y){
 
 void gradient_descent(double **x,double **x_train,double **x_test,double *w,double **w_history,int *y){
     int i,j,iteration;
-    double sum;
+    double sum,accuracy_rate;
     int result[40];
 
 
@@ -184,12 +212,67 @@ void gradient_descent(double **x,double **x_train,double **x_test,double *w,doub
             }
         }
     }
+
+    sum = 0;
+
+    for(i<0;i<40;i++){
+        sum += result[i];
+    }
+
+    accuracy_rate = sum/40.0;
     
 }
 
-void Adam_Algorithm(double **x,double *w,double **x_train,double **w_history,int *y){
-    double learningRate = 0.001,b1 = 0.9,b2 = 0.999,e = pow(10,-8),gt,mt=0,vt=0;
-    int i,j,iteration;
+void stochastic_gradient(double **x,double **x_train,double **x_test, double *w, double **w_history,int *y){
+    int i,j,iteration,result[40];
+    double sum,accuracy_rate;
+
+    for(i=0;i<785;i++){
+        w_history[0][785] = w[i];
+    }
+    for(iteration = 1;iteration <101;iteration++){
+        stochastic_gradiant_w_edit(w,x_train,y);
+        w_history_edit(w,w_history,iteration);
+    }
+    for(i=0;i<40;i++){
+        sum =0;
+        for(j=0;j<785;j++){
+            sum += x_test[i][j]*w[j]; 
+        }
+
+        if(i<20){
+            
+            if(tanh(sum) >= 0.5){
+                result[i] = 1;
+            }else{
+                result[i] = 0;
+            }
+
+
+        }else{
+            if(tanh(sum) <= -0.5){
+                result[i] = 1;
+            }else{
+                result[i] = 0;
+            }
+        }
+    }
+
+    
+    sum = 0;
+    for(i<0;i<40;i++){
+        sum += result[i];
+    }
+
+    accuracy_rate = sum/40.0;
+
+
+}
+
+
+void Adam_Algorithm(double **x,double *w,double **x_train,double **x_test,double **w_history,int *y){
+    double learningRate = 0.001,b1 = 0.9,b2 = 0.999,e = pow(10,-8),gt,mt=0,vt=0,sum,accuracy_rate;
+    int i,j,iteration,result[40];
 
     for(i=0;i<785;i++){
         w_history[0][785] = w[i];
@@ -216,12 +299,53 @@ void Adam_Algorithm(double **x,double *w,double **x_train,double **w_history,int
 
 
     }
+
+    for(i=0;i<40;i++){
+        sum =0;
+        for(j=0;j<785;j++){
+            sum += x_test[i][j]*w[j]; 
+        }
+
+        if(i<20){
+            
+            if(tanh(sum) >= 0.5){
+                result[i] = 1;
+            }else{
+                result[i] = 0;
+            }
+
+
+        }else{
+            if(tanh(sum) <= -0.5){
+                result[i] = 1;
+            }else{
+                result[i] = 0;
+            }
+        }
+    }
+
+    sum = 0;
+
+    for(i<0;i<40;i++){
+        sum += result[i];
+    }
+
+    accuracy_rate = sum/40.0;
+
     
 }
 
+void edit_x(double **x) {
+    for (int i = 0; i < 200; i++) {
+        for (int j = 0; j < 785; j++) {
+            x[i][j] = x[i][j]/255.0;
+        }
+    }
+}
 
 int main(){
 
+    srand(time(0));
     int i;
     double **x = (double **)calloc(200,sizeof(double *));
     for(i=0;i<200;i++){
@@ -244,12 +368,19 @@ int main(){
         w_history[i] = (double *)calloc(785,sizeof(double));
     }
     int *y = calloc(200,sizeof(int));
-    //const char *filename = "data.csv";
+    const char *filename = "data.csv";
 
+
+    read_csv(filename,x);
+    edit_x(x);
+
+    generate_w(w,0.1);
     y_edit(y);
-    //read_csv(filename, x);
+    
     separation_train_Test(x,x_train,x_test);
     gradient_descent(x,x_train,x_test,w,w_history,y);
+    
+   
     
     printf("calisiyo");
     return 0;
